b20dc3fbbfa3ef81f6e4063d5f512a855162da0f
DevDolphin7 <DevDolphin7@users.noreply.github.com>
2024-03-07 22:07:06 +0000

    Bug Fix Attempt
    Attempt to fix bug where pip does not correctly install package

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.txt").read_text()
 
 setup(
     name='splashscreen_ctk',
-    version='0.2.0',
+    version='0.2.1',
     long_description=long_description,
     long_description_content_type="text/plain"
 )
